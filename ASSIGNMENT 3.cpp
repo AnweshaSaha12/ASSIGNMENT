02d153974ff53bcaa7f38af45c80ea6a42c8d18d
AnweshaSaha12 <sahaanwesha2007@gmail.com>
2025-08-11 21:10:59 +0530

    Add files via upload

---
+++ ASSIGNMENT 3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    float f1,f2,difference;
+    printf("Enter a number f1:\n");
+    scanf("%f",&f1);
+    printf("Enter a number f2:\n");
+    scanf("%f",&f2);
+    difference=(f1-f2);
+    printf("the difference is=%f\n",difference);
+    
+	return 0;
+}
